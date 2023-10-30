261fb5b7b9bdabb7baa873741f5aa4d2b6f3963a
qube-dev-rajkumar <rajkumar.pujari@qubehealth.com>
2023-10-31 01:40:21 +0530

    Updated code for google sheet access

--- app/service/user_service.js
+++ app/service/user_service.js
@@ -1,58 +1,81 @@
 const XLSX = require('xlsx');
 const path = require('path');
+const { google } = require('googleapis');
+
+
+
 
 exports.loginService = async (username, password) => {
-    let userDetailsSheet;
-    let firstSheet;
-    let data;
-    //Lets load google sheet to userDetailsSheet
-    try {
-        userDetailsSheet = XLSX.readFile(path.join(__dirname, '../../assets/', 'User_Details.xlsx'));
-        firstSheet = userDetailsSheet.Sheets[userDetailsSheet.SheetNames[0]];
-        data = XLSX.utils.sheet_to_json(firstSheet);
-    } catch (error) {
-        console.error("Error loading or parsing the user details:", error);
-    }
-    // Now lets add logic to check given user and his password is present in our sheet or not
-    const user = data.find((dataSnapShot) => dataSnapShot.Name === username);
+    const sheets = google.sheets({
+        version: 'v4',
+        auth: "AIzaSyCebfm4YDwfDQXxCyzZwwir38twYiJbujY",
+    });
 
-    if (!user || user.Password != password) {
+    const spreadsheetId = '1sgYw2HVXryX29OP6dqmkqdk-9j3NfuQycJ0-RsNHIQA';
+    const range = 'userdetails!A:B';  // Specify the sheet and range you want to read
+
+    // Get the spreadsheet data.
+    const response = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range
+    });
+
+    const values = response.data.values;
+    const user = values.find((dataSnapShot) => dataSnapShot[0] === username);
+
+    if (!user || user[1] !== password.toString()) {
         return {
             "status": user ? 401 : 404,
             "message": user ? "Please enter the correct password." : "User is not registered. Please register the user first.",
             "data": null
         };
     }
+
     return {
         "status": 200,
         "message": "Login successful",
         "data": user
-    }
-}
+    };
+};
+
+
+
 
 exports.getSchemeService = async (username) => {
-    let userSchemeSheet;
-    let schemeMappingSheet;
-    let firstUserSchemeSheet;
-    let firstSchemeMappingSheet;
-    let schemeData;
-    let mappingData;
-    //Lets load google sheet to userDetailsSheet
-    try {
-        userSchemeSheet = XLSX.readFile(path.join(__dirname, '../../assets/', 'Scheme_Details.xlsx'));
-        schemeMappingSheet = XLSX.readFile(path.join(__dirname, '../../assets/', 'User Scheme Mapping Details.xlsx'));
 
-        firstUserSchemeSheet = userSchemeSheet.Sheets[userSchemeSheet.SheetNames[0]];
-        firstSchemeMappingSheet = schemeMappingSheet.Sheets[schemeMappingSheet.SheetNames[0]];
 
-        schemeData = XLSX.utils.sheet_to_json(firstUserSchemeSheet);
-        mappingData = XLSX.utils.sheet_to_json(firstSchemeMappingSheet);
+    const sheets = google.sheets({
+        version: 'v4',
+        auth: "AIzaSyCebfm4YDwfDQXxCyzZwwir38twYiJbujY",
+    });
+
+    const schemeDetailsId = '1q5wJ98MbTmrs8WSiv2DHqBYR7S1vaDFrYKIk9eJRZ6c';
+    const schemeDetailsRange = 'scheme_details!A:B';
+
+    const userSchemeId = '1WjtienWENsFi_Q2Jnb_5r4Bt9QetPBloPeYP74LBgHQ';
+    const userSchemeRange = 'user_scheme!A:B';
+
+    // Get the spreadsheet data.
+    const schemeDetailsResponse = await sheets.spreadsheets.values.get({
+        spreadsheetId: schemeDetailsId,
+        range: schemeDetailsRange
+    });
+
+    const userSchemeResponse = await sheets.spreadsheets.values.get({
+        spreadsheetId: userSchemeId,
+        range: userSchemeRange
+    });
+
+
+    const schemeDetailsValues = schemeDetailsResponse.data.values.slice(1);
+
+    const userSchemeValues = userSchemeResponse.data.values.slice(1);
+    console.log(userSchemeValues);
         //getting user from mapping sheet
-        const user = mappingData.find((dataSnapShot) => dataSnapShot.UserName === username);
-        //getting scheme for this user
-        const enrolledScheme = schemeData.filter((dataSnapShot) => dataSnapShot.SchemeName === user.SchemaName.trim());
-        //getting available other scheme
-        const availableScheme = schemeData.filter((dataSnapShot) => dataSnapShot.SchemeName != user.SchemaName.trim());
+    const user = userSchemeValues.find((dataSnapShot) => dataSnapShot[0] === username);
+    const enrolledScheme = schemeDetailsValues.filter((dataSnapShot) => dataSnapShot[0] === user[1].trim());
+    const availableScheme = schemeDetailsValues.filter((dataSnapShot) => dataSnapShot[0] !== user[1].trim());
+
 
 
         if (enrolledScheme) {
@@ -67,9 +90,7 @@ exports.getSchemeService = async (username) => {
             message: "Data Not Found",
             data: null
         }
-    } catch (error) {
-        console.error("Error loading or parsing the user details:", error);
-    }
+
 }
 
 
